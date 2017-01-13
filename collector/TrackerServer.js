9965a4d80af79f44c235bd1e339d27a7d1ebe86e
Vladimir Nachev <vladimir.penkov.nachev@gmail.com>
2017-01-13 20:05:44 +0200

    Accept requests, that contain required headers Reindent

--- collector/TrackerServer.js
+++ collector/TrackerServer.js
@@ -4,33 +4,44 @@ var mongodb = require('mongodb');
 var mongoClient = mongodb.MongoClient;
 var headers_keys = ['latitude', 'longitude', 'altitude', 'satelites_count', 'localization_time', 'battery_rate'];
 
+function filter_request(request){
+    var result = true;
+    for (var header in headers_keys){
+        result = result && request.headers[headers_keys[header]] !== undefined;
+    }
+    return result;
+}
 
 var server = http.createServer(function(request, response){
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	console.log(request.headers);
-	mongoClient.connect("mongodb://localhost/GPSTracker",
-		function(err, database){
-			if(err){
-				console.error("Error occured:", error);
-				return;
-			}
-			var tracker = database.collection("tracker");
-			var location = {};
-			for (var key in headers_keys) {
-				location[headers_keys[key]] = request.headers[headers_keys[key]];
-			}
-			location["submit_date"] = new Date();
-			tracker.insert(location,
-				function(err, result){ 
-					if (err) { 
-						console.error(err);
-						return 
-					} 
-					//console.log("Result", result)
-				});
-		}
-	);
-	response.end("\n");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    console.log(request.headers);
+    if(filter_request(request) === false){
+        console.log("The request does not contain required headers\n");
+        return;
+    }
+    mongoClient.connect("mongodb://localhost/GPSTracker",
+        function(err, database){
+            if(err){
+                console.error("Error occured:", error);
+                return;
+            }
+            var tracker = database.collection("tracker");
+            var location = {};
+            for (var key in headers_keys) {
+                location[headers_keys[key]] = request.headers[headers_keys[key]];
+            }
+            location["submit_date"] = new Date();
+            tracker.insert(location,
+                function(err, result){
+                    if (err) {
+                        console.error(err);
+                        return
+                    }
+                    //console.log("Result", result)
+                });
+        }
+    );
+    response.end("\n");
 });
 
 server.listen(80);
